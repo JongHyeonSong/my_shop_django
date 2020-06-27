88ada8f0936d66683c3f612d1f6d6a07e06b72f9
JongHyeonSong <mapelamll@naver.com>
2020-06-27 18:53:20 +0900

    #10 주문생성/삭제 페이지 만들고 장고폼을 쓰면서 post를 분기처리함

--- accounts/views.py
+++ accounts/views.py
@@ -1,9 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http import HttpResponse
 from .models import *
+from .forms import *
 
 def home(request):
-    orders = Order.objects.all()
+    orders = Order.objects.all().order_by('-id')
     customers = Customer.objects.all()
     
     total_customers = customers.count()
@@ -40,3 +41,44 @@ def customer(request,pk):
         'order_count':order_count,
     }
     return render(request, 'accounts/customer.html', context)
+
+
+def createOrder(request):
+    form = OrderForm()
+    if request.method=="POST":
+        form =OrderForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('/')
+        
+    context={
+        'form':form,
+    }
+    return render(request, 'accounts/order_form.html',context)
+
+def updateOrder(request,pk):
+    
+    order = Order.objects.get(id=pk)
+    if request.method=="POST":
+        form =OrderForm(request.POST,instance=order)
+        if form.is_valid():
+            form.save()
+            return redirect('/')
+        
+    form = OrderForm(instance=order)
+    context={
+       'form':form,
+    }
+    return render(request, 'accounts/order_form.html',context)
+
+def deleteOrder(request,pk):
+    order = Order.objects.get(id=pk)
+    
+    if request.method=="POST":
+        order.delete()
+        return redirect('/')
+    
+    context={
+        'item':order,
+             }
+    return render(request, 'accounts/delete.html', context)
